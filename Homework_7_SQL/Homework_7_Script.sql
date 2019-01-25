a5496782624e636c9fa6ec778ff215242ac1342d
sponre01 <sponre01@gmail.com>
2019-01-24 21:29:22 -0600

    updated homework 7

--- Homework_7_SQL/Homework_7_Script.sql
+++ Homework_7_SQL/Homework_7_Script.sql
@@ -18,28 +18,44 @@ WHERE first_name = "Joe";
 -- 2b. Find all actors whose last name contain the letters GEN:
 SELECT actor_id, first_name, last_name 
 FROM actor 
-WHERE last_name contains "GEN";
+WHERE last_name like '%GEN%';
 
 -- 2c. Find all actors whose last names contain the letters LI. This time, order the rows by last name and first name, in that order:
-
+SELECT actor_id, first_name, last_name 
+FROM actor 
+WHERE last_name like '%LI%'
+ORDER BY last_name, first_name;
 
 -- 2d. Using IN, display the country_id and country columns of the following countries: Afghanistan, Bangladesh, and China:
-
+SELECT country_id, country
+FROM country 
+WHERE country IN ("Afghanistan", "Bangladesh", "China");
 
 -- 3a. You want to keep a description of each actor. You don't think you will be performing queries on a description, 
 -- so create a column in the table actor named description and use the data type BLOB 
 -- (Make sure to research the type BLOB, as the difference between it and VARCHAR are significant).
-
+ALTER TABLE actor
+ADD description BLOB(100);
+SELECT *
+FROM actor;
 
 -- 3b. Very quickly you realize that entering descriptions for each actor is too much effort. Delete the description column.
-
+ALTER TABLE actor
+DROP COLUMN description;
+SELECT *
+FROM actor;
 
 -- 4a. List the last names of actors, as well as how many actors have that last name.
-
+SELECT last_name 'Last_Name', COUNT(0) 'Total_Actors'
+FROM actor
+GROUP BY last_name;
 
 -- 4b. List last names of actors and the number of actors who have that last name, 
 -- but only for names that are shared by at least two actors
-
+SELECT last_name 'Last_Name', COUNT(0) 'Total_Actors'
+FROM actor
+GROUP BY last_name
+HAVING COUNT(0) > 1;
 
 -- 4c. The actor HARPO WILLIAMS was accidentally entered in the actor table as GROUCHO WILLIAMS. Write a query to fix the record.
 
@@ -51,29 +67,71 @@ WHERE last_name contains "GEN";
 -- 5a. You cannot locate the schema of the address table. Which query would you use to re-create it?
 
 
--- 6a. Use JOIN to display the first and last names, as well as the address, of each staff member. Use the tables staff and address:
 
+-- 6a. Use JOIN to display the first and last names, as well as the address, of each staff member. Use the tables staff and address:
+SELECT staff.first_name, staff.last_name, address.address
+FROM staff
+JOIN address ON
+staff.address_id=address.address_id;
 
 -- 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment.
-
+SELECT staff.first_name, staff.last_name, SUM(payment.amount)
+FROM staff
+JOIN payment ON
+staff.staff_id=payment.staff_id;
 
 -- 6c. List each film and the number of actors who are listed for that film. Use tables film_actor and film. Use inner join.
-
+SELECT film.title, COUNT(film_actor.actor_id) 'number_of_actors'
+FROM film_actor
+INNER JOIN film
+ON film.film_id = film_actor.film_id
+GROUP BY film.film_id
+;
 
 -- 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
-
+SELECT COUNT(inventory_id)
+FROM inventory
+WHERE film_id in
+(
+	SELECT film_id
+	FROM film
+	WHERE title = "Hunchback Impossible"
+);
 
 -- 6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer. 
 -- List the customers alphabetically by last name:
-
+SELECT customer.customer_id, customer.last_name, customer.first_name, SUM(payment.amount) 'total_paid'
+FROM customer
+JOIN payment ON
+payment.customer_id=customer.customer_id
+GROUP BY customer.customer_id
+ORDER BY customer.last_name;
 
 -- 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with 
 -- the letters K and Q have also soared in popularity. Use subqueries to display the titles of movies starting with the letters K and Q 
 -- whose language is English.
-
+	SELECT title
+	FROM film
+	WHERE title like 'Q%' or title like 'K%' and language_id in
+		(
+		SELECT language_id
+		FROM language
+		WHERE name = "English"
+);
 
 -- 7b. Use subqueries to display all actors who appear in the film Alone Trip.
-
+SELECT Concat(first_name, " ",last_name) 'Actor_Name'
+FROM actor
+WHERE actor_id in
+	(
+	SELECT actor_id
+	FROM film_actor
+	WHERE film_id in
+		(
+		SELECT film_id
+		FROM film
+		WHERE title = "Alone Trip"
+));
 
 -- 7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all 
 -- Canadian customers. Use joins to retrieve this information.
