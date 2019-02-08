0190cd40ce05816351c1fd5d7f042a04f4d52b35
sponre01 <sponre01@gmail.com>
2019-02-07 18:44:52 -0600

    homework 8 non-optional complete

--- Homework_8_Advanced-Data-Storage-and-Retrieval/app.py
+++ Homework_8_Advanced-Data-Storage-and-Retrieval/app.py
@@ -41,11 +41,10 @@ def welcome():
     return (
         f"Welcome to the Homework 8 Flask App!<br/>"
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation"
-        f"/api/v1.0/stations"
-        f"/api/v1.0/tobs"
-        f"/api/v1.0/<start>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/yyyy-mm-dd/yyyy-mm-dd<start>/<end><br/>"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -58,6 +57,7 @@ def precipitation():
         prcp_dict["date"] = measurement.date
         prcp_dict["precipitation"] = measurement.prcp
         precipitation.append(prcp_dict)
+    session.close()
     return jsonify(precipitation)
 
 @app.route("/api/v1.0/stations")
@@ -65,6 +65,7 @@ def stations():
     """Return a JSON list of stations from the dataset."""
     results = session.query(Station.name).all()
     all_names = list(np.ravel(results))
+    session.close()
     return jsonify(all_names)
 
 
@@ -76,18 +77,17 @@ def tobs():
     filter(Measurement.date > year_ago).\
     order_by(Measurement.date).\
     group_by(Measurement.date).all()
+    session.close()
     return jsonify(tobs_data)
 
 @app.route("/api/v1.0/<start>/<end>")
-def startend():
+def startend(start,end):
     """Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range."""
-    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-    tobs_data = session.query(Measurement.date, func.avg(Measurement.tobs)).\
-    filter(Measurement.date > year_ago).\
-    order_by(Measurement.date).\
-    group_by(Measurement.date).all()
-
-    return jsonify(tobs_data)
+    startend_data = session.query(func.max(Measurement.tobs), func.avg(Measurement.tobs), func.min(Measurement.tobs)).\
+    filter(Measurement.date > start).\
+    filter(Measurement.date < end).all()
+    session.close()
+    return jsonify(startend_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
